c7baaf214f0e60b86a975ba1bde9b2088628a5b3
GuidoSaborio <guido.saborio.r@gmail.com>
2025-12-01 20:24:33 -0600

    Refactor river density calculation in RiverDensity.R
    Updated river density calculation using OSM data and WorldClim reference.

--- RiverDensity.R
+++ RiverDensity.R
@@ -1,29 +1,50 @@
+## Código basado en:
+## https://stackoverflow.com/questions/69035297/calculating-road-density-raster-from-road-shapefile
+
+library(terra)
+library(sf)
+
+rm(list=ls())
+gc()
+
 ### DENSIDAD DE RIOS###
+
+## Utilizar la variables Bio1 de WoldClim como referencia para la resolución
+bio1 <- terra::rast("source/bio1_23.bil")
+cr <- st_read("source/bordecr2008polycrtm05.shp", crs = 5367)
+cr <- st_transform(cr, crs = "WGS84")
+bio1 <- crop(bio1, cr)
+bio1 <- mask(bio1, cr)
+
+## Utilizar capa de ríos de OSM
 rios <- st_read("source/CR_RiosOSM.shp")
 
-v1 <- vect(rios)
-rios1 <- as.lines(v1)
-rs1 <- rast(v1) 
+v <- vect(rios)
+rios <- as.lines(v)
+rs <- rast(v, nrows=381, ncols=411, nlyrs=1, xmin=-84.68333, xmax=-82.70833, 
+           ymin=8.041667, ymax=10.10833, crs= "+proj=longlat +datum=WGS84 +no_defs") 
 
-values(rs1) <- 1:ncell(rs1)
-names(rs1) <- "rast"    
-rsp1 <- as.polygons(rs1)
+values(rs) <- 1:ncell(rs)
+names(rs) <- "rast"    
+rsp <- as.polygons(rs)
 
-rp1 <- intersect(rios1, rsp1)
+rp <- intersect(rios, rsp)
 
-rp1$length <- perim(rp1) / 1000 #km
-x1 <- tapply(rp1$length, rp1$rast, sum)
+rp$length <- perim(rp) / 1000 #km
+x <- tapply(rp$length, rp$rast, sum)
 
-rios1 <- rast(rs1)
-rios1[as.integer(names(x1))] <- as.vector(x1)
-
+rios_r <- rast(rs)
+rios_r[as.integer(names(x))] <- as.vector(x)
+rios_r[is.na(rios_r)]<- 0
+plot(rios_r)
 
 #Denrios <- project(rios, bio1)
-riosF <- resample(rios1, bio1)
+riosF <- resample(rios_r, bio1)
 riosF <- crop(riosF, bio1)
 riosF <- mask(riosF, bio1)
-writeRaster(riosF, "results/CR_RiosDensity.asc",overwrite=TRUE)
 
+plot(riosF)
+writeRaster(riosF, "results/CR_RiosDensity.asc",overwrite=TRUE)
 
 
 
